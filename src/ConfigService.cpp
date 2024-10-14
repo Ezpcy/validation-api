dcb77b4797a1a422bdcf79372db0e29ebc0a2526
Ezpcy <batu.s@live.com>
2024-10-14 16:07:30 +0200

    Config handling init

--- src/ConfigService.cpp
+++ src/ConfigService.cpp
@@ -6,19 +6,20 @@
 #include <boost/filesystem/operations.hpp>
 #include <unordered_map>
 #include <validation-api/ConfigService.hpp>
+#include <validation-api/
 
 namespace validation_api {
 
 ConfigService::ConfigService() = default;
 
-// Get a configuration with a shared lock (allows concurrent reads)
+// Get a configuration with a shared lock (allows concurren:q
+// t reads)
 nlohmann::json ConfigService::getConfig(std::string &name) {
   boost::shared_lock<boost::shared_mutex> lock(_rwMutex_);  // Shared lock
   auto it = _configs_.find(name);
   if (it != _configs_.end()) {
-    nlohmann::json jsonObject;
-    for (p) }
-  return nlohmann::json();  // Return empty if not found
-}
+    xmlToJson();
+    return nlohmann::json();  // Return empty if not found
+  }
 
 };  // namespace validation_api
